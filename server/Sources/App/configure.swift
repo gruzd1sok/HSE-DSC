45d3409039c81d3bf2dbbc107f5712ab89982894
Ilya Gruzdev <ily.gruzdev99@gmail.com>
2022-06-14 18:37:23 +0300

    fix
    fix

--- server/Sources/App/configure.swift
+++ server/Sources/App/configure.swift
@@ -7,7 +7,7 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
-    app.databases.use(.postgres(hostname: "localhost", username: "postgres", password: "lol", database: "hse"), as: .psql)
+    app.databases.use(.postgres(hostname: "localhost", username: "gruzdev", password: "gruzdev", database: "gruzdev_2"), as: .psql)
     app.migrations.add(CreateNumber())
     
     // register routes
